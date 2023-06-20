d5bae093150831dd31e02410ddd85a89c498af17
daniel-yeh <daniel-yeh@meepshop.com>
2023-06-20 16:28:10 +0800

    create projects

---
+++ libs/shared/ui/src/lib/button/button.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta } from '@storybook/react';
+import Button from './button';
+
+const Story: Meta<typeof Button> = {
+  component: Button,
+  title: 'Button',
+};
+export default Story;
+
+export const Primary = {
+  args: {
+    text: 'Click me!',
+    padding: 10,
+    disabled: true,
+  },
+};
